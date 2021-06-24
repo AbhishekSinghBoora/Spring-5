412fde5960dc826c462e850e777c0b6c74b48a60
Abhishek Singh <Abhishek Singh@KIRITO>
2021-06-24 12:38:30 +0530

    Updated the project from Spring to Spring Boot

--- EmployeeManagementApplication/src/main/java/com/infy/Client.java
+++ EmployeeManagementApplication/src/main/java/com/infy/Client.java
@@ -1,17 +1,17 @@
 package com.infy;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.AbstractApplicationContext;
 
-import com.infy.dto.EmployeeDTO;
 import com.infy.service.EmployeeServiceImpl;
-import com.infy.util.SpringConfiguration;
-
+@SpringBootApplication
 public class Client {
 
 	public static void main(String[] args) {
-		AbstractApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfiguration.class);
-		EmployeeServiceImpl service = (EmployeeServiceImpl) ctx.getBean("employeeService");
+		EmployeeServiceImpl service = null;
+		AbstractApplicationContext ctx = (AbstractApplicationContext) SpringApplication.run(Client.class, args) ;
+		service = (EmployeeServiceImpl) ctx.getBean("employeeService");
 		System.out.println(service);
 		ctx.close();
 
